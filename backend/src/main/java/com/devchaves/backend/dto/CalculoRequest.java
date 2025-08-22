4d3223021286a38cefaff213a74f7036ba97ac15
chaves <chavesprogrammer@gmail.com>
2025-08-22 17:05:10 -0300

    dto e inicio da service

--- backend/src/main/java/com/devchaves/backend/dto/CalculoRequest.java
+++ backend/src/main/java/com/devchaves/backend/dto/CalculoRequest.java
@@ -1,4 +1,28 @@
 package com.devchaves.backend.dto;
 
-public record CalculoRequest() {
+import com.devchaves.backend.entity.Anexo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+
+import java.time.LocalDate;
+
+public record CalculoRequest(
+
+        @NotBlank
+        @Pattern(regexp = "\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}", message = "CNPJ inválido")
+        String cnpj,
+
+        @NotBlank
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        @PastOrPresent(message = "A data deve ser no passado ou presente")
+        LocalDate mesReferencia,
+        @NotBlank(message = "RPA não pode ser vazio")
+        String rpa,
+        @NotBlank(message = "RBT12 não pode ser vazio")
+        String rbt12,
+        @NotBlank(message = "Anexo não pode ser vazio")
+        Anexo anexo
+) {
 }
--- backend/src/main/java/com/devchaves/backend/service/CalculoService.java
+++ backend/src/main/java/com/devchaves/backend/service/CalculoService.java
@@ -1,4 +1,24 @@
 package com.devchaves.backend.service;
 
+import com.devchaves.backend.dto.CalculoRequest;
+import com.devchaves.backend.util.BigDecimalUtil;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Service
 public class CalculoService {
+
+    public BigDecimal calculoDasModeloPreReforma(CalculoRequest dto){
+
+        BigDecimal rpa = BigDecimalUtil.toMonetario(dto.rpa());
+        BigDecimal rbt12 = BigDecimalUtil.toMonetario(dto.rbt12());
+
+
+        BigDecimal aliquotaAfetiva = new BigDecimal(BigInteger.ONE);
+
+        return null;
+    }
+
 }
