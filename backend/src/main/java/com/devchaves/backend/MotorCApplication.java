7ff3f1961dd870c4ab9e633c6be36c23f311529f
chaves <chavesprogrammer@gmail.com>
2025-08-22 09:28:38 -0300

    inicio

---
+++ backend/src/main/java/com/devchaves/backend/MotorCApplication.java
@@ -0,0 +1,13 @@
+package com.devchaves.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorCApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devchaves/backend/MotorCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchaves.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
