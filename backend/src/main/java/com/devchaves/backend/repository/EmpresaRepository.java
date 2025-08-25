8ab631489c70de2a93cfac66e8b46029334b1380
chaves <chavesprogrammer@gmail.com>
2025-08-25 14:16:43 -0300

    repositories

--- backend/src/main/java/com/devchaves/backend/repository/EmpresaRepository.java
+++ backend/src/main/java/com/devchaves/backend/repository/EmpresaRepository.java
@@ -4,9 +4,11 @@ import com.devchaves.backend.entity.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 
-
+    Optional<Empresa> findByCNPJ(String cnpj);
 
 }
--- backend/src/main/java/com/devchaves/backend/repository/FaixaRepository.java
+++ backend/src/main/java/com/devchaves/backend/repository/FaixaRepository.java
@@ -2,8 +2,18 @@ package com.devchaves.backend.repository;
 
 import com.devchaves.backend.entity.FaixaAnexo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @Repository
 public interface FaixaRepository extends JpaRepository<FaixaAnexo, Long> {
+
+    @Query(value = "SELECT f FROM FaixaAnexo WHERE f.anexo_id = :anexo_id AND" +
+            ":rbt12 BETWEEN f.receitaBrutaMin AND f.receitaBruxaMax")
+    Optional<FaixaAnexo> faixaIdealDeCalculo(@Param("anexo_id")Long id);
+
 }
